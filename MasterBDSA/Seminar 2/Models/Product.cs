b6336abbc927efbb08fa2eaa927d252065616962
razboinic096 <razboinic096@yahoo.com>
2023-10-01 16:41:47 +0300

    add shopping

--- MasterBDSA/Seminar 2/Models/Product.cs
+++ MasterBDSA/Seminar 2/Models/Product.cs
@@ -20,8 +20,8 @@
             var products = new List<Product>();
 
             products.Add(new Product { Id = 1, Name = "Product1", Description = "D1", ImagePath = "img/img1.PNG", IsAvailable = true, Price = 20 });
-            products.Add(new Product { Id = 1, Name = "Product2", Description = "D2", ImagePath = "img/img2.PNG", IsAvailable = true, Price = 50 });
-            products.Add(new Product { Id = 1, Name = "Product3", Description = "D3", ImagePath = "img/img3.PNG", IsAvailable = true, Price = 100 });
+            products.Add(new Product { Id = 2, Name = "Product2", Description = "D2", ImagePath = "img/img2.PNG", IsAvailable = true, Price = 50 });
+            products.Add(new Product { Id = 3, Name = "Product3", Description = "D3", ImagePath = "img/img3.PNG", IsAvailable = true, Price = 100 });
 
             return products;
         }
--- MasterBDSA/Seminar 2/Program.cs
+++ MasterBDSA/Seminar 2/Program.cs
@@ -3,6 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +26,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
