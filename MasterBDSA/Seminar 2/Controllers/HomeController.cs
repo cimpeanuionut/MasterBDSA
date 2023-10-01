9f8e9acd0b0f7420f7f12212c516fee47c0d97b7
razboinic096 <razboinic096@yahoo.com>
2023-10-01 16:09:52 +0300

    home page seminar 2

---
+++ MasterBDSA/Seminar 2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Seminar_2.Models;
+
+namespace Seminar_2.Controllers
+{
+    public class HomeController : Controller
+    { 
+        public HomeController()
+        {           
+        }
+
+        public IActionResult Index()
+        {
+            return View(Product.GetAll());
+        }       
+    }
+}
